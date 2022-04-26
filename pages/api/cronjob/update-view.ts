0cc68ff358423342188d6db4c59c1936e5f409c0
hudy9x <hudy9x@gmail.com>
2022-04-26 11:07:35 +0700

    chore: format code

--- pages/api/cronjob/update-view.ts
+++ pages/api/cronjob/update-view.ts
@@ -11,9 +11,6 @@ interface ISnippetCounter {
   };
 }
 
-type FirebaseDoc =
-  FirebaseFirestore.DocumentReference<FirebaseFirestore.DocumentData>;
-
 function log(content: string) {
   console.log(content);
   logtail.info(content);
@@ -69,59 +66,6 @@ export default async function handler(
       });
 
       const total = snippets.length;
-      // for (let index = 0; index < snippets.length; index++) {
-      //   const snippet = snippets[index];
-      //   const updateViewPromise = (async (snippet, index) => {
-      //     try {
-      //       await firestore.runTransaction(async (transaction) => {
-      //         log(
-      //           `=> ${index + 1}/${total} Update view of snippets/${snippet}`
-      //         );
-
-      //         const groupSnippetItem = groupSnippets[snippet];
-      //         const snippetRef = await transaction.get(
-      //           firestore.doc(`snippets/${snippet}`)
-      //         );
-
-      //         if (!snippetRef.exists) {
-      //           // throw `Document snippets/${snippet} does not exist!`;
-      //           log(`Document snippets/${snippet} does not exist!`);
-      //           return 0;
-      //         }
-
-      //         const snippetData = snippetRef.data() as ISnippet;
-      //         const newTotalView = groupSnippetItem.totalView;
-      //         const viewIdOfSnippet = groupSnippetItem.viewIds;
-      //         const oldView = snippetData.view || 0;
-
-      //         log(`Update total view in snippets/${snippet}`);
-
-      //         transaction.update(snippetRef.ref, {
-      //           view: newTotalView + oldView,
-      //         });
-
-      //         log(`Mark view as done`);
-      //         const markAsDonePromises: FirebaseFirestore.Transaction[] = [];
-      //         viewIdOfSnippet.forEach((viewId) => {
-      //           markAsDonePromises.push(
-      //             transaction.update(firestore.doc(`views/${viewId}`), {
-      //               done: true,
-      //             })
-      //           );
-      //         });
-
-      //         await Promise.all(markAsDonePromises);
-      //         return 1;
-      //       });
-      //     } catch (error) {
-      //       log(
-      //         `=> ${
-      //           index + 1
-      //         }/${total} Update view of snippets/${snippet} ERROR`
-      //       );
-      //     }
-      //   })(snippet, index);
-      // }
 
       const snippetPromises = snippets.map(async (snippet, index) => {
         try {
@@ -170,69 +114,10 @@ export default async function handler(
 
       await Promise.all(snippetPromises);
 
-      // snippets.forEach(async (snippet, index) => {
-      //   try {
-      //     await firestore.runTransaction(async (transaction) => {
-      //       log(`=> ${index + 1}/${total} Update view of snippets/${snippet}`);
-
-      //       const groupSnippetItem = groupSnippets[snippet];
-      //       const snippetRef = await transaction.get(
-      //         firestore.doc(`snippets/${snippet}`)
-      //       );
-
-      //       if (!snippetRef.exists) {
-      //         // throw `Document snippets/${snippet} does not exist!`;
-      //         log(`Document snippets/${snippet} does not exist!`);
-      //         return;
-      //       }
-
-      //       const snippetData = snippetRef.data() as ISnippet;
-      //       const newTotalView = groupSnippetItem.totalView;
-      //       const viewIdOfSnippet = groupSnippetItem.viewIds;
-      //       const oldView = snippetData.view || 0;
-
-      //       log(`Update total view in snippets/${snippet}`);
-
-      //       transaction.update(snippetRef.ref, {
-      //         view: newTotalView + oldView,
-      //       });
-
-      //       log(`Mark view as done`);
-      //       viewIdOfSnippet.forEach(async (viewId) => {
-      //         await transaction.update(firestore.doc(`views/${viewId}`), {
-      //           done: true,
-      //         });
-      //       });
-      //     });
-      //   } catch (error) {
-      //     log(
-      //       `=> ${index + 1}/${total} Update view of snippets/${snippet} ERROR`
-      //     );
-      //   }
-      // });
-
       res.status(200).json({ message: "Success" });
     })
-    .then()
     .catch((err) => {
       log("Get views ERROR");
       res.status(500).json({ message: err });
     });
-
-  // if (req.method === "POST") {
-  //   try {
-  //     // const { authorization } = req.headers;
-  //     // if (authorization === `Bearer ${process.env.API_SECRET_KEY}`) {
-  //     //   res.status(200).json({ success: true });
-  //     // } else {
-  //     //   res.status(401).json({ success: false });
-  //     // }
-  //   } catch (err) {
-  //     res.status(500).json({ statusCode: 500, message: err.message });
-  //   }
-  // } else {
-  //   res.status(200).json({ method: "GET", success: true });
-  //   // res.setHeader('Allow', 'POST');
-  //   // res.status(405).end('Method Not Allowed');
-  // }
 }
